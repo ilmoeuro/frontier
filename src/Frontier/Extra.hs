a8a0ed1b79ba97f1d2b1d047cfed6932c79ecafc
ilmo.euro <ilmo.euro@gmail.com>
2014-12-17 08:26:00 +0200

    ** Non-Empty List in Extra

--- src/Frontier/Extra.hs
+++ src/Frontier/Extra.hs
@@ -1,7 +1,18 @@
+{-# LANGUAGE DeriveFoldable    #-}
+{-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE DeriveTraversable #-}
 module Frontier.Extra
     (headOr
     ,singleOr
     ) where
+
+import Data.Foldable
+import Data.Traversable
+
+data NEL a
+    = One a
+    | And a (NEL a)
+    deriving (Show, Eq, Functor, Foldable, Traversable)
 
 headOr :: [a] -> a -> a
 headOr (x:_) _ = x
