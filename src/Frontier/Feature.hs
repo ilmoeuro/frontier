e4ec8f2842f6031c6ea8421115751b34af94c456
ilmo.euro <ilmo.euro@gmail.com>
2014-12-18 10:53:18 +0200

    ** Simple composition, modules as functions

--- src/Frontier/Feature.hs
+++ src/Frontier/Feature.hs
@@ -1,17 +1,30 @@
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE GADTs              #-}
+{-# LANGUAGE ImpredicativeTypes #-}
+{-# LANGUAGE RankNTypes         #-}
 module Frontier.Feature
-    (Feature(..)
+    (Query(..)
+    ,Feature
+    ,Feature'
+    ,comp
     ) where
 
+import Control.Arrow
 import Frontier.Feature.Action (ActionM)
 import Frontier.Feature.Qualifier
 
-data Feature a = Feature
-    {initItems              :: [a Item]
-    ,symbol                 :: a Object -> Char
-    ,command                :: Char -> Maybe (a (Action ()))
-    ,initPlayerCharacter    :: a Object
-    -- TODO: way to compare non-enum things
-    ,eq                     :: forall b. a b -> a b -> Bool
-    ,run                    :: forall b. a (Action b) -> ActionM a b
-    }
+data Query a result where
+    InitItems               :: Query a [a Item]
+    Symbol                  :: a Object -> Query a Char
+    InitPlayerCharacter     :: Query a (a Object)
+    Command                 :: Char -> Query a (a (Action ()))
+    Eq                      :: (forall b. a b -> a b) -> Query a Bool
+    -- TODO: Polymorphic
+    Run                     :: a (Action ()) -> Query a (ActionM a ())
+
+type Feature' a = forall result. Query a result -> result
+type Feature a b c d = (Feature' a, b -> c -> d)
+
+comp :: (b -> c, b1 -> c1)
+        -> (b' -> c')
+        -> ((b, b') -> (c, c'), (b'1 -> c'1) -> (b1, b'1) -> (c1, c'1))
+comp (ftr,k) ftr' = (ftr *** ftr', (k ***))
